ae65bd5ee5ba120d647ec15a95a3d7ff9a1994ef
lordyhas <dev.haspro@gmail.com>
2023-03-22 23:22:54 +0100

    version 1.3.22

--- contents/article_abstract.php
+++ contents/article_abstract.php
@@ -39,7 +39,7 @@ if(!isset($_GET['id'])){
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
-                            <h1>Article Astract</h1>   
+                            <h1>Article Abstract</h1>
                             <span class="subheading">A Blog by lordyhas</span>
                         </div>
                     </div>
