e6d22324208802c4d4a74743a75589defbd4110a
lordyhas <dev.haspro@gmail.com>
2024-09-05 23:54:00 -0400

    work on course ui | version 1.8.0

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -14,42 +14,45 @@
         </div>
     </div>
 
+
+
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h3 class="mb-3  text-xl"><b>{{ __("Sommaire") }}</b></h3>
+                    <h2 class="mb-3  text-2xl"><b>{{ __("Sommaire") }}</b></h2>
                     <div>
-                        <table class="table">
+                        <table class="table text-start" >
                             <tbody>
                             <tr>
-                                <th scope="row">{{"Session"}}</th>
-                                <td>{{""}}</td>
-                                <td>{{""}}</td>
+                                <th class="col-6 text-start">{{"Session"}}</th>
+                                <td class="col-2">{{"Aucune"}}</td>
+                                <td class="col-2">{{""}}</td>
+                            </tr>
+
+
+                            <tr>
+                                <th class="col-6 text-start pr-5">Enseignant</th>
+                                <td class="col-2">{{""}}</td>
+                                <td class="col-2">{{""}}</td>
                             </tr>
 
                             <tr>
-                                <th scope="row">Enseignant</th>
-                                <td>{{""}}</td>
-                                <td>{{""}}</td>
+                                <th class="col-6 text-start">Période de cours</th>
+                                <td class="col-2">{{""}}</td>
+                                <td class="col-2">{{""}}</td>
                             </tr>
 
                             <tr>
-                                <th scope="row">Période de cours</th>
-                                <td>{{""}}</td>
-                                <td>{{""}}</td>
+                                <th class="col-6 text-start pr-5">Période de disponibilités </th>
+                                <td class="col-2 ">Par Teams ou sur demande pour une rencontre en personne.</td>
+                                <td class="col-2">{{""}}</td>
                             </tr>
 
                             <tr>
-                                <th scope="row">Période de disponibilités</th>
-                                <td>Par Teams ou sur demande pour une rencontre en personne.</td>
-                                <td>{{""}}</td>
-                            </tr>
-
-                            <tr>
-                                <th scope="row">Absences</th>
-                                <td>Aucune</td>
-                                <td>{{""}}</td>
+                                <th class="col-6 text-start">Absences</th>
+                                <td class="col-2">Aucune</td>
+                                <td class="col-2">{{""}}</td>
                             </tr>
 
                             </tbody>
@@ -60,42 +63,65 @@
         </div>
     </div>
 
+    <div class="pb-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div>
+                        <strong>420-7A4-FE Environnement Web</strong>
+                        <ul>
+                            <li>Pondération : 2-2-2</li>
+                            <li>2,0 unités</li>
+                        </ul>
+
+                        Le préalable absolu à ce cours :<br>
+                        &emsp;&minus; <em>420-705-FE Programmation des interfaces Web</em><br>
+
+                        Ce cours est préalable absolu au cours suivant :<br>
+                        &emsp;&minus; <em>420-706-FE Projet intégrateur</em>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <style>
+        .expand-col {
+            width: 100%;
+        }
+
+        .expand-col-60 {
+            width: 60%;
+        }
+
+        .expand-col-20 {
+            width: 20%;
+        }
+    </style>
+
     @if(true)
         <div class="py-6">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6">
-                        <h3 class="mb-3  text-xl"><b>{{ __("Notes et support de cours") }}</b></h3>
+                        <h3 class="mb-3  text-2xl"><b>{{ __("Notes et support de cours") }}</b></h3>
 
                         <div>
                             <table class="table">
-                                <!-- thead class="thead-light">
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Nom</th>
-                                    <th scope="col">Contact</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Address</th>
-                                    <th scope="col">Actions</th>
-                                </tr>
-                                </thead -->
                                 <tbody>
                                     <tr>
-                                        <th scope="row">01. Introduction au cours</th>
-                                        <td>Diapo</td>
-                                        <td>{{""}}</td>
+                                        <th class="expand-col-60 text-start">01. Introduction au cours</th>
+                                        <td class="expand-col-20">Diapo <i class="fas fa-download"></i></td>
+                                        <td class="expand-col-20">{{"PDF"}}</td>
                                         <td>{{""}}</td>
                                     </tr>
 
                                     <tr>
-                                        <th scope="row">02. </th>
+                                        <th class="text-start">02. </th>
                                         <td>Diapo</td>
                                         <td>{{""}}</td>
                                         <td>{{""}}</td>
                                     </tr>
-
-
-
 
                                 </tbody>
                             </table>
